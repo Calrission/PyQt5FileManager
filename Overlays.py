e33406ab6ed64d0b5e94ca6d42690552c73beac5
Calrission <va2005art@gmail.com>
2022-10-24 23:05:43 +0300

    add alpha design version QActionPathObject

--- Overlays.py
+++ Overlays.py
@@ -7,6 +7,7 @@ class QOverlay(QWidget):
     def __init__(self, x: int, y: int, parent: QWidget):
         self.x = x
         self.y = y
+        self.background = None
         super().__init__(parent)
 
     def _calc_x_y(self, x, y):
@@ -15,12 +16,20 @@ class QOverlay(QWidget):
         self.x, self.y = new_x, new_y
 
     def show(self) -> None:
+        self.initUI()
         self.move(self.x, self.y)
         self.raise_()
         super().show()
 
     def initUI(self):
         pass
+
+    def _init_background(self, w: int, h: int):
+        self.background = QLabel(self)
+        UtilsVisual.load_file_to_label_with_scaled("files/back_tag_select.png", self.background,
+                                                   (w, h))
+        self.background.resize(w, h)
+        self.background.lower()
 
     def __repr__(self):
         return f"{self.__class__.__name__}({self.x=}, {self.y=})"
@@ -29,15 +38,36 @@ class QOverlay(QWidget):
 class QActionPathObject(QOverlay):
     def __init__(self, path_object: PathObject, x: int, y: int, parent: QWidget):
         super().__init__(x, y, parent)
-        self.icon = None
         self.path_object = path_object
 
     def initUI(self):
-        self.resize(100, 100)
-        self.icon = QLabel(self)
-        UtilsVisual.load_file_to_label_with_scaled("files/icon", self.icon, (self.width(), self.height()))
+        open_label = QLabel(self)
+        open_label.setText("Открыть")
 
-    def show(self):
-        self.initUI()
-        super().show()
+        pre_open_label = QLabel(self)
+        pre_open_label.setText("Предпросмотр")
+
+        rename_label = QLabel(self)
+        rename_label.setText("Переименовать")
+
+        delete_label = QLabel(self)
+        delete_label.setText("Удалить")
+
+        info_label = QLabel(self)
+        info_label.setText("Свойства")
+
+        open_label.setStyleSheet("QLabel { color: rgb(255, 255, 255); }")
+        pre_open_label.setStyleSheet("QLabel { color: rgb(255, 255, 255); }")
+        rename_label.setStyleSheet("QLabel { color: rgb(255, 255, 255); }")
+        delete_label.setStyleSheet("QLabel { color: rgb(255, 255, 255); }")
+        info_label.setStyleSheet("QLabel { color: rgb(255, 255, 255); }")
+
+        open_label.move(5, 0)
+        pre_open_label.move(5, 20)
+        rename_label.move(5, 40)
+        delete_label.move(5, 60)
+        info_label.move(5, 80)
+
+        super().initUI()
+        self._init_background(80, 100)
 
