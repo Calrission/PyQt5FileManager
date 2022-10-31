554c8f68b81a95cb47f426e3306fe39553645be5
Kavrese <va2005art@gmail.com>
2022-10-31 18:22:04 +0300

    add script for count lines in py files

---
+++ count_lines.py
@@ -0,0 +1,9 @@
+import os
+lst = [i for i in os.listdir(os.path.realpath("/".join(str(__file__).split("/")[0:-1])))
+       if os.path.isfile(i) and i != "count_lines.py"]
+count = 0
+for file in lst:
+    with open(file, "r") as f:
+        count += len(f.read().split("\n"))
+
+print(count)
