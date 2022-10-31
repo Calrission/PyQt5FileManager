24bf4b58721bff6a8b3d1f8bbb6b5ec872f85e7c
Kavrese <va2005art@gmail.com>
2022-10-31 18:58:43 +0300

    and filter not py files in script count_lines.py

--- count_lines.py
+++ count_lines.py
@@ -1,6 +1,6 @@
 import os
 lst = [i for i in os.listdir(os.path.realpath("/".join(str(__file__).split("/")[0:-1])))
-       if os.path.isfile(i) and i != "count_lines.py"]
+       if os.path.isfile(i) and i != "count_lines.py" and ".py" in i]
 count = 0
 for file in lst:
     with open(file, "r") as f:
