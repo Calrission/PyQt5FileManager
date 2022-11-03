be8a356c16e6f153700c97ebb5fdad1d43ef8a99
Calrission <kavrese@gmail.com>
2022-11-03 20:38:44 +0300

    fix image PreviewsManager.py

--- managers/PreviewsManager.py
+++ managers/PreviewsManager.py
@@ -1,8 +1,10 @@
 from previews.QPathObjectPreview import QPathObjectPreview
+from widgets.QAlphaDarkBackground import QAlphaDarkBackground
 
 
-class PreviewsManager:
+class PreviewsManager(QAlphaDarkBackground):
     def __init__(self):
+        super().__init__()
         self.active_preview = None
 
     def show_preview(self, preview: QPathObjectPreview):
@@ -10,9 +12,15 @@ class PreviewsManager:
             self.dismiss_active_preview()
 
         self.active_preview = preview
+        self.show_background()
         preview.show()
 
     def dismiss_active_preview(self):
         if self.active_preview is not None:
             self.active_preview.deleteLater()
             self.active_preview = None
+
+    def press_background(self, *args):
+        super().press_background(*args)
+        self.dismiss_active_preview()
+
