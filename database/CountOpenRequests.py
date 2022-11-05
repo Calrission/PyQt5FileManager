363817505cfce6a74814b7db60f49a3c9600a384
Calrission <kavrese@gmail.com>
2022-11-05 16:50:31 +0300

    add support methods to CountOpenRequests.py

--- database/CountOpenRequests.py
+++ database/CountOpenRequests.py
@@ -4,9 +4,16 @@ from sqlite3 import Cursor, Connection
 
 class CountOpenRequests:
     @push_query_db_decorator
-    def get_all_dict(self, cursor: Cursor, conn: Connection):
-        result = cursor.execute("select * from count_open").fetchall()
+    def get_all_dict_desc(self, cursor: Cursor, conn: Connection):
+        result = cursor.execute("select * from count_open order by count desc").fetchall()
         return {i[1]: i[2] for i in result}
+
+    def get_all_dict_limit(self, limit: int):
+        return self.get_all_dict_desc()[:limit]
+
+    def get_max_count_path(self):
+        data = self.get_all_dict_desc()
+        return list(data.keys())[0]
 
     @push_query_db_decorator
     def update_count(self, path: str, new_count: int, cursor: Cursor, conn: Connection):
